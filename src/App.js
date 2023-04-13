5ce4ffe9feb6b7f405302aec3e35386b34d4deb4
You Dong Eun <ehddms3@sookmyung.ac.kr>
2023-04-13 17:43:13 +0900

    프론트앤드 개발 1~6 React로 안녕하세요! 출력하기

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+class App extends React.Component { 
+    render(){ 
+        return (
+          <div className="App">
+            <Hello />
+          </div>
+        ); 
+    }
+}
+
+export default App;
