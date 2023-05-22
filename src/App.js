377195d248f63f1d9cb5ec3bf2d886015778a15a
You Dong Eun <ehddms3@sookmyung.ac.kr>
2023-05-22 22:30:01 +0900

    프론트앤드 10 React로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -25,29 +25,37 @@ class App extends React.Component {
  console.log("items:",this.state.items);
 
  }
-  render(){ 
-    // todoItems에 this.state.items.length가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다.
-    // totoItems = this.state.items.length > 0 ? (<Paper></Paper>):"" 이렇게 해도 같은 결과이다.
-    // > 조건선택문 ? ternary operator
+// delete 함수 추가. (alt+shift+f 포맷팅)
+  delete =(item)=>{ 
+    const thisItems =this.state.items; 
+    const newItems = thisItems.filter(e => e.id !== item.id); 
+    this.setState({items:newItems},()=>{ 
+      //디버깅 콜백
+      console.log("Update Items : ",this.state.items)
+    }); 
+  }
 
-    var todoItems = this.state.items.length >0 && (
+  render(){ 
+    // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다. 
+    // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
+    var todoItems =this.state.items.length >0 &&(
       <Paper style={{margin:16}}>
         <List>
         {this.state.items.map((item,idx)=>(
-        <Todo item={item} key={item.id}/>
-        ))}
+        <Todo item={item} key={item.id} delete={this.delete}/>
+        ))} 
         </List>
       </Paper>
-    ); 
-  
-  // (2) add 함수 연결 
-    return (
-    <div className="App">
-      <Container maxWidth="md">
-        <AddTodo add={this.add}/>
-        <div className="TodoList">{todoItems}</div>
-      </Container>
-    </div>
+    );
+ 
+     // 생성된 컴포넌트 JSX를 리턴한다. 
+     return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/>
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
     );
  }
 }
